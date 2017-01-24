a492388c92f6c067293966d779254d2702e6bfff
Sam Denton <samwyse@gmail.com>
2017-01-24 09:18:53 -0600

    tweak the test names

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -24,67 +24,67 @@ class TestFactory(TestCase):
         print('done')
         return p
 
-    def test_Offline_BNF_50(self):
+    def test_Offline_BNF_big_enough(self):
         p = self._prepare(PackingMode.Offline, PackingBin.BNF, 50, 50)
         # check that bins were created
         self.assertGreater(len(p.bin_list()), 0)
         # check that all of the rectangles made it in
         self.assertEqual(len(p.rect_list()), len(self.rectangles))
 
-    def test_Offline_BNF_5(self):
+    def test_Offline_BNF_too_small(self):
         p = self._prepare(PackingMode.Offline, PackingBin.BNF, 5, 5)
         # check that none of the rectangles made it in
         self.assertEqual(len(p.rect_list()), 0)
 
-    def test_Offline_BFF_50(self):
+    def test_Offline_BFF_big_enough(self):
         p = self._prepare(PackingMode.Offline, PackingBin.BFF, 50, 50)
         # check that bins were created
         self.assertGreater(len(p.bin_list()), 0)
         # check that all of the rectangles made it in
         self.assertEqual(len(p.rect_list()), len(self.rectangles))
 
-    def test_Offline_BFF_5(self):
+    def test_Offline_BFF_too_small(self):
         p = self._prepare(PackingMode.Offline, PackingBin.BFF, 5, 5)
         # check that none of the rectangles made it in
         self.assertEqual(len(p.rect_list()), 0)
 
-    def test_Offline_BBF_50(self):
+    def test_Offline_BBF_big_enough(self):
         p = self._prepare(PackingMode.Offline, PackingBin.BBF, 50, 50)
         # check that bins were created
         self.assertGreater(len(p.bin_list()), 0)
         # check that all of the rectangles made it in
         self.assertEqual(len(p.rect_list()), len(self.rectangles))
 
-    def test_Offline_BBF_5(self):
+    def test_Offline_BBF_too_small(self):
         p = self._prepare(PackingMode.Offline, PackingBin.BBF, 5, 5)
         # check that none of the rectangles made it in
         self.assertEqual(len(p.rect_list()), 0)
 
 # TODO:  The following test isn't working, len(p.rect_list()) is 0.
-##    def test_Offline_Global_50(self):
+##    def test_Offline_Global_big_enough(self):
 ##        p = self._prepare(PackingMode.Offline, PackingBin.Global, 50, 50)
 ##        # check that bins were created
 ##        self.assertGreater(len(p.bin_list()), 0)
 ##        # check that all of the rectangles made it in
 ##        self.assertEqual(len(p.rect_list()), len(self.rectangles))
 
-    def test_Offline_Global_5(self):
+    def test_Offline_Global_too_small(self):
         p = self._prepare(PackingMode.Offline, PackingBin.Global, 5, 5)
         # check that none of the rectangles made it in
         self.assertEqual(len(p.rect_list()), 0)
 
 
 def helper():
-    """create a bunch of tests to copy and paste above"""
+    """create a bunch of tests to copy and paste into TestFactory"""
     for mode in PackingMode:
         for bin_algo in PackingBin:
-            for size in '50', '5':
+            for size, w, h in ('big_enough', 50, 50), ('too_small', 5, 5):
                 name = '_'.join(('test', mode, bin_algo, size))
                 print("""\
     def %s(self):
         p = self._prepare(PackingMode.%s, PackingBin.%s, %s, %s)""" %
-                      (name, mode, bin_algo, size, size))
-                if size == '50':
+                      (name, mode, bin_algo, w, h))
+                if size == 'big_enough':
                     print("""\
         # check that bins were created
         self.assertGreater(len(p.bin_list()), 0)
