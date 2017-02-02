57e0e57b5e5a88274f10dbfc074816a916e57108
secnot <secnot@secnot.com>
2017-02-02 11:16:52 +0100

    Added test_stats tests

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -1,16 +1,38 @@
 import random 
 from unittest import TestCase
-import matplotlib.pyplot as plt
 from collections import defaultdict
+from timeit import default_timer as timer
+import rectpack
+
+
+from  rectpack import GuillotineBssfSas, GuillotineBssfLas, \
+    GuillotineBssfSlas, GuillotineBssfLlas, GuillotineBssfMaxas, \
+    GuillotineBssfMinas, GuillotineBlsfSas, GuillotineBlsfLas, \
+    GuillotineBlsfSlas, GuillotineBlsfLlas, GuillotineBlsfMaxas, \
+    GuillotineBlsfMinas, GuillotineBafSas, GuillotineBafLas, \
+    GuillotineBafSlas, GuillotineBafLlas, GuillotineBafMaxas, \
+    GuillotineBafMinas
+
+from rectpack import MaxRectsBl, MaxRectsBssf, MaxRectsBaf, MaxRectsBlsf
+
+from rectpack import SkylineMwf, SkylineMwfl, SkylineBl, \
+    SkylineBlWm, SkylineMwfWm, SkylineMwflWm
+
+from rectpack import PackingMode, PackingBin
+
+
+# For repeatable rectangle generation
+random.seed(4)
+
 
 def coherce_to(maxn, minn, n):
-    assert maxn > minn
+    assert maxn >= minn
     return max(min(maxn, n), minn)
 
 
 def random_rectangle(max_side, min_side, sigma=0.5, ratio=1.0, coherce=True):
 
-    assert min_side < max_side
+    assert min_side <= max_side
 
     #
     half_side = (max_side-min_side)/2
@@ -32,12 +54,73 @@ def random_rectangle(max_side, min_side, sigma=0.5, ratio=1.0, coherce=True):
     return width, height
 
 
-class TestRandomRectangles(TestCase):
+RECTANGLES = [random_rectangle(40, 20, ratio=1.2) for _ in range(100)]\
+        +[random_rectangle(20, 15, ratio=1.2) for _ in range(100)]
+BINS = [random_rectangle(200, 200, ratio=1.5) for _ in range(3)]
 
-    def test_generation(self):
-        rects = [random_rectangle(50, 10, ratio=1.5) for _ in range(9900)]
 
-        #average 
-        width_avg  = sum((r[0] for r in rects))
-        height_avg = sum((r[1] for r in rects))
+class TestWastedSpace(TestCase):
+
+    def setUp(self):
+        self.rectangles = [(int(w), int(h)) for w, h in RECTANGLES]
+        self.bins = [(int(w), int(h)) for w, h in BINS]
+        self.packer = None
+        self.algos = [
+            GuillotineBssfSas, GuillotineBssfLas, GuillotineBssfSlas, \
+            GuillotineBssfLlas, GuillotineBssfMaxas, GuillotineBssfMinas, \
+            GuillotineBlsfSas, GuillotineBlsfLas, GuillotineBlsfSlas, \
+            GuillotineBlsfLlas, GuillotineBlsfMaxas, GuillotineBlsfMinas, \
+            GuillotineBafSas, GuillotineBafLas, GuillotineBafSlas, \
+            MaxRectsBl, MaxRectsBssf, MaxRectsBaf, MaxRectsBlsf, \
+            SkylineBl, SkylineBlWm, SkylineMwfl, SkylineMwf]
+
+        self.bin_algos = [
+            (rectpack.PackingBin.BNF, "BNF"),
+            (rectpack.PackingBin.BFF, "BFF"),
+            (rectpack.PackingBin.BBF, "BBF"),
+            (rectpack.PackingBin.Global, "GLOBAL"),
+        ]
+    @staticmethod
+    def first_bin_wasted_space(packer):
+        bin1 = packer[1] 
+        bin_area = bin1.width*bin1.height
+        rect_area = sum((r.width*r.height for r in bin1))    
+        return ((bin_area-rect_area)/bin_area)*100
+
+
+    @staticmethod
+    def packing_time(packer):
+        start = timer()
+        packer.pack()
+        end = timer()
+        return end-start
+
+    @staticmethod
+    def setup_packer(packer, bins, rectangles):
+        for r in rectangles:
+            packer.add_rect(*r)
+        for b in bins:
+            packer.add_bin(*b)
+
+        return packer
+
+    def test_modes(self):
+        for bin_algo, algo_name in self.bin_algos:
+            for algo in self.algos:
+                packer = rectpack.newPacker(pack_algo=algo, bin_algo=bin_algo)
+                self.setup_packer(packer, self.bins, self.rectangles)
+                time = self.packing_time(packer)
+                self.first_bin_wasted_space(packer)
+                wasted = self.first_bin_wasted_space(packer)
+
+                # Test wasted spaced threshold
+                """print("{0} {1:<20s} {2:>10.3f}s {3:>10.3f}% {4:>10} bins".format(
+                    algo_name, algo.__name__, time, wasted, len(packer)))"""
+                self.assertTrue(wasted<50)
+
+                # Validate rectangle packing
+                for b in packer:
+                    b.validate_packing()
+
+
 
