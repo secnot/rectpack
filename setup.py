427e01f1b573d1681da1040a8c4a9a18d9569736
secnot <secnot@secnot.com>
2017-02-22 14:19:12 +0100

    Added long_description

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
+long_description="""A collection of heuristic algorithms for solving the 2D knapsack problem,
+also known as the bin packing problem. In essence packing a set of rectangles into the 
+smallest number of bins."""
 
 setup(
     name='rectpack',
     version='0.2',
-    description='2D Rectangle packing library',
-    
+    description=long_description, 
+
     # Main homepage
     url='https://github.com/secnot/rectpack/',
     
