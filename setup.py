39c162505f0559fa12991fd58ae3ab4da779e509
joaquin <j>
2021-11-23 14:07:34 -0800

    remove empty lines

--- setup.py
+++ setup.py
@@ -1,36 +1,25 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-long_description="""A collection of heuristic algorithms for solving the 2D knapsack problem,
-also known as the bin packing problem. In essence packing a set of rectangles into the 
+long_description = """A collection of heuristic algorithms for solving the 2D knapsack problem,
+also known as the bin packing problem. In essence packing a set of rectangles into the
 smallest number of bins."""
 
 setup(
-    name='rectpack',
-    version='0.2.2',
-    description=long_description, 
-
-    # Main homepage
-    url='https://github.com/secnot/rectpack/',
-    
-    # Extra info and author details
-    author='SecNot',
-
-    keywords=['knapsack', 'rectangle', 'packing 2D', 'bin', 'binpacking'],
-
-    license='Apache-2.0',
-
+    name="rectpack",
+    version="0.2.2",
+    description=long_description,
+    url="https://github.com/secnot/rectpack/",
+    author="SecNot",
+    keywords=["knapsack", "rectangle", "packing 2D", "bin", "binpacking"],
+    license="Apache-2.0",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: Apache Software License',
-        ],
-
-    # package
-    packages = ['rectpack'],
-    zip_safe = False,
-
-    # Tests
-    test_suite='nose.collector',
-    tests_require=['nose'],
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    packages=["rectpack"],
+    zip_safe=False,
+    test_suite="nose.collector",
+    tests_require=["nose"],
 )
